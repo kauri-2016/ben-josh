243638b4b01be56e04799c75512f6a2d860d7118
EDA <eda@mac-mini-3.local>
2016-05-19 11:21:33 +1200

    added comments

--- game.js
+++ game.js
@@ -3,12 +3,11 @@ module.exports = {
 }
 
 function scoreFrame (frame, nextFrame) {
- if (typeof(frame[1]) === 'undefined') {
-   return 10 + nextFrame[0] + nextFrame[1]
-
- }
+  if (typeof (frame[1]) === 'undefined') { // strike
+    return 10 + nextFrame[0] + nextFrame[1]
+  }
   var score = frame[0] + frame[1]
-  if (score === 10) {
+  if (score === 10) { // spare
     return 10 + nextFrame[0]
   }
   return score
