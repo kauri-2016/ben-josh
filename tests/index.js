515a2fa9dab35445fc7b4aa3d75ed94a99862794
EDA <eda@mac-mini-3.local>
2016-05-19 10:38:13 +1200

    scoring open frames

--- tests/index.js
+++ tests/index.js
@@ -12,3 +12,10 @@ test('test for gutterball', function (t) {
   t.equal(score, 0, 'score a gutterball frame')
   t.end()
 })
+
+test('scores an open frame', function (t) {
+  var frame = [2, 6]
+  var score = game.scoreFrame(frame)
+  t.equal(score, 8, 'scores an open frame')
+  t.end()
+})
